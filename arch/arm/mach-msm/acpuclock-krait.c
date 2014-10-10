857f0ab4958f95c3cc7e12023f63464b4a1a64ed
flar2 <asegaert@gmail.com>
2014-10-09 21:58:04 -0400

    voltage control
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	arch/arm/mach-msm/acpuclock-krait.c

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -996,7 +996,11 @@ static void __init bus_init(const struct l2_level *l2_level)
 
 #ifdef CONFIG_CPU_VOLTAGE_TABLE
 
+<<<<<<< HEAD
 #define HFPLL_MIN_VDD		 700000
+=======
+#define HFPLL_MIN_VDD		 800000
+>>>>>>> 4e1e6bf... voltage control
 #define HFPLL_MAX_VDD		1350000
 
 ssize_t acpuclk_get_vdd_levels_str(char *buf) {
